2de585cab440e728ea62fdff21bbfbe87cdd8aae
Aleksandra <Sasha.kabanova.777@mail.ru>
2026-04-24 12:14:26 +0300

    .

---
+++ src/lab05/strategies.py
@@ -0,0 +1,57 @@
+# ключи для сортировки
+def by_price(item):
+    """сортировка по цене"""
+    return item.price
+
+def by_area(item):
+    """Сортировка по площади"""
+    return item.area
+
+def by_per_metr(item):
+    """Сортировка по цене за м^2"""
+    return item.price / item.area
+# ================================
+
+# фильтры
+def is_expensive(item):
+    """Дорогие объекты > 30 млн"""
+    return item.prive > 30_000_000
+
+def is_cheap(item):
+    """Дешевые объекты < 30 млн"""
+    return item.price <= 30_000_000
+
+def is_for_rent(item):
+    """Объекты доступные для аренды"""
+    return item.for_rent
+# ====================================
+
+
+# фабрика функций
+def make_prise_filter(max_price):
+    """Создает фильтр по макс. цене"""
+    def filter_fn(item):
+        return item.price <= max_price
+    return filter_fn
+# ====================================
+
+# для map
+def get_price(item):
+    """Получить цену"""
+    return item.price
+
+def to_str(item):
+    """Преобразовывает в строку"""
+    return str(item)
+# =================================
+
+class DiscountStrategy:
+    """Стратегия СКИДКА"""
+    def __call__(self, item):
+        return item.price * 0,2
+
+class TaxStrategy:
+    """Стратегия НАЛОГ"""
+    def __call__(self, item):
+        return item.price * 0,1
+# ================================
